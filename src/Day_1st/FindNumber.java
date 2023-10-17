6d86252191037216d934dbfa7b87e79123724054
Rushi Pisole <rushikeshpisole3@gmail.com>
2023-10-18 02:46:59 +0530

    Day 1

--- src/Day_1st/FindNumber.java
+++ src/Day_1st/FindNumber.java
@@ -1,8 +1,21 @@
 package Day_1st;
 
-public class FindNumber {
-    public static void main(String[] args){
-        System.out.println("1st");
-    }
+import java.util.Scanner;
 
+public class FindNumber {
+    public static void main (String[]args)
+
+    {
+
+      Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number: ");
+        int num = sc.nextInt();
+        if (num > 0)
+            System.out.println("The number is positive");
+        else if (num < 0)
+            System.out.println("The number is negative");
+        else
+            System.out.println("Zero");
+
+    }
 }
--- src/Day_2/Hello.java
+++
@@ -1,8 +0,0 @@
-package Day_2;
-
-public class Hello {
-    public static void main(String[] args){
-        System.out.println("1st");
-    }
-
-}
