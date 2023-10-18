13e74a775b67f4bb41bf59a6030d13c0d692308b
Rushi Pisole <rushikeshpisole3@gmail.com>
2023-10-19 00:49:07 +0530

    Day 2

--- src/Day_2/GreatestNumber.java
+++ src/Day_2/GreatestNumber.java
@@ -6,7 +6,7 @@ public class GreatestNumber {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Number: ");
+        System.out.println("Enter Numbers: ");
         int num1 = sc.nextInt();
         int num2 = sc.nextInt();
         if (num1 > num2) System.out.println("Number 1 is the Greater");
