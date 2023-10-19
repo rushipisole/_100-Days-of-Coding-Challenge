366c7323676c7393f8aeeac14cdd2f02c849bedd
Rushi Pisole <rushikeshpisole3@gmail.com>
2023-10-20 01:23:43 +0530

    Day 3

---
+++ src/Day_3/SumOfDigitOfNumber.java
@@ -0,0 +1,18 @@
+package Day_3;
+
+import java.util.Scanner;
+
+public class SumOfDigitOfNumber {
+    public static void main(String[] args) {
+        int  sum = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter The Numbers: ");
+        int num = sc.nextInt();
+
+        while (num != 0) {
+            sum += num % 10;
+            num = num / 10;
+            System.out.println("The Sum of all Digits is:" + sum);
+        }
+    }
+}
