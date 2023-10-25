b925635dd0153937c647b75e9fe269e95ad3cc08
Rushi Pisole <rushikeshpisole3@gmail.com>
2023-10-25 22:48:55 +0530

    Day 6

--- src/Day_6/LargestNumberInArrays.java
+++ src/Day_6/LargestNumberInArrays.java
@@ -3,7 +3,7 @@ package Day_6;
 public class LargestNumberInArrays {
     public static void main(String args[]) {
 
-        int arr[] = {20, 67, 69, 106, 54, 45, 18, 1};
+        int arr[] = {20, 67, 69, 106, 54, 45, 18, 10};
 
         int max = arr[0];
 
