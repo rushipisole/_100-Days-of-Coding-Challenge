2a9c2a7b5484049934fef0661ee9e66cdabb7e1c
Rushi Pisole <rushikeshpisole3@gmail.com>
2023-10-27 23:12:28 +0530

    Day 8

---
+++ src/Day_8/SumInArrays.java
@@ -0,0 +1,18 @@
+package Day_8;
+
+public class SumInArrays {
+    public static void main(String args[])
+    {
+
+        int arr[] = {12, 13, 1, 10, 34, 10};
+
+        int sum = 0;
+
+        for(int i=0; i<arr.length; i++)
+        {
+            sum = sum + arr[i];
+        }
+
+        System.out.print(sum);
+    }
+}
