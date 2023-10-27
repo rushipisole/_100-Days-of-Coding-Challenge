6276cd3056b7c8771439997448b81f78b55340a6
Rushi Pisole <rushikeshpisole3@gmail.com>
2023-10-27 23:13:49 +0530

    Day 8

--- src/Day_8/ReverseInArrays.java
+++ src/Day_8/ReverseInArrays.java
@@ -3,7 +3,7 @@ package Day_8;
 public class ReverseInArrays {
     public static void main(String args[]) {
 
-        int arr[] = {10, 20, 30, 40, 50};
+        int arr[] = {10, 20, 30, 40, 100};
 
         int n = arr.length;
         for (int i = n - 1; i >= 0; i--)
