fd2c76fb1d234772c061be16a3adc9a12c8a61d0
Rushi Pisole <rushikeshpisole3@gmail.com>
2023-10-29 23:05:55 +0530

    Day 10

---
+++ src/Day_10/PowerOfNumber.java
@@ -0,0 +1,16 @@
+package Day_10;
+
+public class PowerOfNumber {
+    public static void main (String[]args)
+    {
+        int base = 5, x = 3;
+        System.out.println ("Required Power is " + power (base, x));
+    }
+    //Recursive Function
+    static int power (int base, int x)
+    {
+        if (x == 0)     //Base Condition
+            return 1;
+        return (base * power (base, x - 1));
+    }
+}
