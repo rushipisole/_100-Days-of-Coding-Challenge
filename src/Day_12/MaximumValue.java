79edc1d094ef468326910eb0df970b6702621e2a
Rushi Pisole <rushikeshpisole3@gmail.com>
2023-10-31 18:12:07 +0530

    Day 12

---
+++ src/Day_12/MaximumValue.java
@@ -0,0 +1,20 @@
+package Day_12;
+
+public class MaximumValue {public static void main(String[] args) {
+    int[] arr = {89, 34, 876, 23, 5346, 67, 9};
+    System.out.println(min(arr));
+}
+
+    static int min(int[] arr) {
+        int ans = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > ans) {
+                ans = arr[i];
+
+            }
+
+        }
+        return ans;
+    }
+
+}
