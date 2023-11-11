7820ae9fe1c03d6825581f284d1eb3e8661dcc96
Rushi Pisole <rushikeshpisole3@gmail.com>
2023-11-11 22:26:47 +0530

    Day 21

---
+++ src/Day_21/perfectSquare.java
@@ -0,0 +1,23 @@
+package Day_21;
+
+public class perfectSquare {
+    static boolean isPerfectSquare(int x) {
+        if (x >= 0) {
+
+            int sr = (int) Math.sqrt(x);
+
+            return ((sr * sr) == x);
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int x = 84;
+
+        if (isPerfectSquare(x))
+            System.out.print("True");
+        else
+            System.out.print("False");
+    }
+}
+
