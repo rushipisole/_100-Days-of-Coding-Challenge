bbd1fe515a3a8c23d2cd6163fa102b784725d4ca
Rushi Pisole <rushikeshpisole3@gmail.com>
2023-12-02 14:54:27 +0530

    Day 28

--- src/Day_28/FindtheHighestAltitude.java
+++ src/Day_28/FindtheHighestAltitude.java
@@ -4,18 +4,19 @@ import java.util.Arrays;
 
 public class FindtheHighestAltitude {
     public static void main(String[] args) {
-        int[] arr = {-4,-3,-2,-1,4,3,2};
+        int[] arr = {-4, -3, -2, -1, 4, 3, 2};
         System.out.println(Arrays.toString(new int[]{value(arr)}));
 
     }
-   static int value(int[] gain){
-    int max = 0;
-    int current = 0;
 
-        for(int i = 0; i < gain.length; i++){
-        current += gain[i];
-        max = Math.max(current, max);
-    }
+    static int value(int[] gain) {
+        int max = 0;
+        int current = 0;
+
+        for (int i = 0; i < gain.length; i++) {
+            current += gain[i];
+            max = Math.max(current, max);
+        }
         return max;
-}
+    }
 }
